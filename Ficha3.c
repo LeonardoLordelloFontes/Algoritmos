e546f6775057537269b939468899c7418587a2ce
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-01-05 01:59:32 +0000

    Update Ficha3.c

--- Ficha3.c
+++ Ficha3.c
@@ -350,7 +350,7 @@ int where2 (char *s, THash2 t) {
     if (i == Size && del)  {
         r = -1;
     }
-    else if (strcmp(s, t[(hash_n + i) % Size].chave) == 0 && t[(hash_n + i) % Size].status != Free){
+    else if (strcmp(s, t[(hash_n + i) % Size].chave) == 0 && t[(hash_n + i) % Size].status != Del){
         r = (hash_n + i) % Size;
     }
     return r;
